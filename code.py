3d91783075b72ac5215ba06b7144ca398b27fc6d
sahroush <55985277+sahroush@users.noreply.github.com>
2020-06-22 17:25:58 +0430

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,8 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import time , requests , shutil
 subreddit = "https://www.reddit.com/r/subreddit/" #edit this line
-prev = ""
-webhook = DiscordWebhook(url='webhook url') #edit this line too
 def send(ur):
+    webhook = DiscordWebhook(url='webhook url') #edit this line too
     embed = DiscordEmbed(title='', description='', color=242424)
     embed.set_image(url=ur)
     webhook.add_embed(embed)
@@ -16,7 +15,6 @@ def splitUrl(imageUrl):
         if 'jpg' or 'webm' or 'mp4' or 'gif' or 'gifv' or 'png' in imageUrl:
             return imageUrl.split('/')[-1]
 def fetch():
-    global prev
     subJson = ''
     if subJson:
         url = makeUrl(subJson['data']['after'], subreddit)
@@ -25,10 +23,8 @@ def fetch():
     subJson = requests.get(url, headers={'User-Agent': 'MyRedditScraper'}).json()
     post = subJson['data']['children']
     imageUrl = (post[0]['data']['url'])
-    if(prev!=imageUrl):
-        send(imageUrl)
-        prev = imageUrl
+    send(imageUrl)
     return
 while(1):
     fetch()
-    time.sleep(180)
+    time.sleep(1800)
