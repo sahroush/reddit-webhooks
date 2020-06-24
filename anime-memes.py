97cc9ae7321dbefd53f7e2e79e51b43219ae5352
soroush sahraei <55985277+sahroush@users.noreply.github.com>
2020-06-24 14:56:40 +0430

    Update anime-memes.py

--- anime-memes.py
+++ anime-memes.py
@@ -1,14 +1,14 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
-import time , requests , shutil , random
+import time , requests  , random
 subreddits = ["https://www.reddit.com/r/Animemes/" , "https://www.reddit.com/r/wholesomeanimemes/" , "https://www.reddit.com/r/anime_irl/"];seen=[]
 def send(ur , ttl):
     global seen
-    webhook = DiscordWebhook(url='web hook url') #edit this line
+    webhook = DiscordWebhook(url='') #edit this line
     embed = DiscordEmbed(title=ttl, description='', color=242424)
     embed.set_image(url=ur)
     webhook.add_embed(embed)
     webhook.execute()
-    seen += [ur];time.sleep(3)
+    seen += [ur];time.sleep(1800)
 def makeUrl(afterID, subreddit):
         return subreddit.split('/.json')[0] + "/.json?after={}".format(afterID)
 def fetch(x , y = 0):
@@ -23,6 +23,6 @@ def fetch(x , y = 0):
         send(imageUrl , imageTitle)
 while(1):
     for i in range(3):
-        fetch(i , random.randint(1 , 100000));
+        fetch(i , random.randint(1 , 10000));
     if(len(seen) > 10000):
         seen = []
