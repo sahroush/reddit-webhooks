b5a8c010547e73a8d24470de46dc3268f1124c27
soroush sahraei <55985277+sahroush@users.noreply.github.com>
2020-06-25 21:47:35 +0430

    Update anime-memes.py

--- anime-memes.py
+++ anime-memes.py
@@ -1,14 +1,25 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import time , requests  , random
 subreddits = ["https://www.reddit.com/r/Animemes/" , "https://www.reddit.com/r/wholesomeanimemes/" , "https://www.reddit.com/r/anime_irl/"];seen=[]
+def start():
+    global seen
+    ch = open("cache.txt" , "w+");
+    ch.close()
+    ch = open("cache.txt" , "r+");
+    content = ch.readlines();
+    ch.close()
+    for i in content:
+        seen+=[i[:len(i)-1]]
 def send(ur , ttl):
     global seen
-    webhook = DiscordWebhook(url='') #edit this line
+    webhook = DiscordWebhook(url='webhook's url')  #edit this line
     embed = DiscordEmbed(title=ttl, description='', color=242424)
     embed.set_image(url=ur)
     webhook.add_embed(embed)
     webhook.execute()
-    seen += [ur];time.sleep(1800)
+    seen += [ur];
+    end()
+    time.sleep(1800)
 def makeUrl(afterID, subreddit):
         return subreddit.split('/.json')[0] + "/.json?after={}".format(afterID)
 def fetch(x , y = 0):
@@ -21,8 +32,23 @@ def fetch(x , y = 0):
         fetch(x , y + 1)
     else :
         send(imageUrl , imageTitle)
-while(1):
-    for i in range(3):
-        fetch(i , random.randint(1 , 10000));
-    if(len(seen) > 10000):
-        seen = []
+def run(x = 100 , y = 100 , z = 100):
+    while(x + y + z):
+        end()
+        if(x):
+            x-=1;
+            fetch(0);
+        if(y):
+            y-=1
+            fetch(1)
+        if(z):
+            z-=1;
+            fetch(2);
+def end():
+    global seen
+    ch = open("cache.txt" , "w+");
+    for i in seen:
+        ch.write(i + '\n')
+    ch.close()
+start()
+run()
