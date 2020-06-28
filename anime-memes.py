f92bc262c8b41c8bc84e677cbc9a85aecb0936ad
soroush sahraei <55985277+sahroush@users.noreply.github.com>
2020-06-28 15:00:45 +0430

    Update anime-memes.py

--- anime-memes.py
+++ anime-memes.py
@@ -1,6 +1,7 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import time , requests  , random
-subreddits = ["https://www.reddit.com/r/Animemes/" , "https://www.reddit.com/r/wholesomeanimemes/" , "https://www.reddit.com/r/anime_irl/"];seen=[]
+subreddits = ["https://www.reddit.com/r/Animemes/" , "https://www.reddit.com/r/wholesomeanimemes/" , "https://www.reddit.com/r/anime_irl/"];
+seen = [[] , [] , []]
 def start():
     global seen
     try:
@@ -12,16 +13,16 @@ def start():
     except:
         ch = open("cache.txt" , "w+");
         ch.close()
-def send(ur , ttl):
+def send(ur , ttl , x):
     global seen
     webhook = DiscordWebhook(url='https://discordapp.com/api/webhooks/724540368988798978/Tbdmk14oKJF4CB0Krve8dbTYlwUrBT5adb9iPjB-XeukqqR4udn6dIVtnnUpI4MBZuWX')
     embed = DiscordEmbed(title=ttl, description='', color=242424)
     embed.set_image(url=ur)
     webhook.add_embed(embed)
     webhook.execute()
-    seen += [ur];
-    if (len(seen) > 1000):
-        seen = seen[500:]
+    seen[x] += [ur];
+    if (len(seen[x]) > 1000):
+        seen[x] = []
     #end()
 def makeUrl(afterID, subreddit):
         return subreddit.split('/.json')[0] + "/.json?after={}".format(afterID)
@@ -33,10 +34,10 @@ def fetch(x , y = 0):
         return
     imageUrl = (post[y%len(post)]['data']['url'])
     imageTitle = (post[y%len(post)]['data']['title'])
-    if(imageUrl in seen or not('jpg' in imageUrl or 'webm' in imageUrl or 'mp4' in imageUrl or 'gif' in imageUrl or 'gifv' in imageUrl or 'png' in imageUrl)):
+    if(imageUrl in seen[x] or not('jpg' in imageUrl or 'webm' in imageUrl or 'mp4' in imageUrl or 'gif' in imageUrl or 'gifv' in imageUrl or 'png' in imageUrl)):
         fetch(x , y + 1)
     else :
-        send(imageUrl , imageTitle)
+        send(imageUrl , imageTitle , x)
 def run():
     while(1):
         #end()
